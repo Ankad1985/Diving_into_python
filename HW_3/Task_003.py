023937f53a8069e9876da47179023140d9d38ace
Kadirov Andrei <kadyrov_andrei@mail.ru>
2023-10-18 14:29:08 +0300

    Добавлены исправления в задачу с рюкзаком

--- HW_3/Task_003.py
+++ HW_3/Task_003.py
@@ -3,21 +3,30 @@
 # вещи влезут в рюкзак передав его максимальную
 # грузоподъёмность. Достаточно вернуть один допустимый вариант.
 
-things = {
-    'Еда': 7,
-    'Пивко': 20,
-    'Спальник': 2,
-    'Палатка': 3
+items = {
+    'ключи': 0.3,
+    'кошелек': 0.2,
+    'телефон': 0.5,
+    'зажигалка': 0.1
 }
-max_weight = 30
-backpack = []
+max_weight = 1
+backpack = {}
 total_weight = 0
 
-for i, weight in things.items():
-    if total_weight + weight <= max_weight:
-        backpack.append(i)
-        total_weight += weight
-# print(backpack)
-print('Берем с собой: ')
-for i in backpack:
-    print(i)
+for item, weight in items.items():
+    if weight <= max_weight:
+        backpack[item] = weight
+        max_weight -= weight
+
+    
+print(backpack)
+
+
+# for i, weight in things.items():
+#     if total_weight + weight <= max_weight:
+#         backpack.append(i, weight)
+#         total_weight += weight
+
+# print('Берем с собой: ')
+# for i in backpack:
+#     print(i)
